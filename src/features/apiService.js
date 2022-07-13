29f43403d0076114fa3038494053daf4c7bfe063
Merdjan Tekfidan <merttekfidan@gmail.com>
2022-07-13 23:05:23 +0200

    Remove proxy, add api url

--- src/features/apiService.js
+++ src/features/apiService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = '/api/v1'
+const API_URL = 'https://infinite-dusk-39113.herokuapp.com/api/v1'
 //http://localhost:8080/api/v1/get-transactions-by-wallet/0xa800f34505e8b340cf3ab8793cb40bf09042b28f
 const getTransfersFromWallet = async (walletAddress) => {
     const response = await axios.get(`${API_URL}/get-transactions-from-wallet/${walletAddress}`)
